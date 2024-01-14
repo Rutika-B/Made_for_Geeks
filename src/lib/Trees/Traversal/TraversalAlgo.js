a7330f84e9da172c89eba98352167d8c2aa2f170
Rutika <rutikabhuimbar@gmail.com>
2024-01-14 16:48:07 +0530

    animated graph

--- src/lib/Trees/Traversal/TraversalAlgo.js
+++ src/lib/Trees/Traversal/TraversalAlgo.js
@@ -3,49 +3,73 @@ This is used to get expected traversal order given the tree structure
 */
 
 function Node(val) {
-    this.value = val;
-    this.left = null;
-    this.right = null;
+  this.value = val;
+  this.left = null;
+  this.right = null;
+}
+
+function Tree(rootNode) {
+  this.root = null;
+  this.visited = []; //keeps the sequence of the nodes in which they are visited
+
+  if (typeof rootNode !== "undefined") {
+    this.root = new Node(rootNode);
+  } else {
+    throw new Error("Enter valid root element. Root node can't be empty");
   }
-  
-  function Tree(rootNode) {
-    this.root = null;
-    this.visited = []; //keeps the sequence of the nodes in which they are visited
-  
-    if (typeof rootNode !== "undefined") {
-      this.root = new Node(rootNode);
-    } else {
-      throw new Error("Enter valid root element. Root node can't be empty");
-    }
-    /*Inserts a new node to the tree. if targetnode is provided then adds the newNode to its left/right based on the value of direction
+  /*Inserts a new node to the tree. if targetnode is provided then adds the newNode to its left/right based on the value of direction
     provided as an argument and if not provided then set it as roots left or right child*/
-    this.add = function (value, dir, targetNode = null) {
-      const newNode = new Node(value);
-  
-      if (targetNode === null) {
-        this.root[dir] = newNode;
-      } else {
-        const position = this.levelorderTraversal(targetNode); //returns the position of the targetNode
-        position[dir] = newNode;
-      }
-    };
-  
-    /*performs level order traversal. If given any node as an argument then return that node's position else
+  this.add = function (value, dir, targetNode = null) {
+    const newNode = new Node(value);
+
+    if (targetNode === null) {
+      this.root[dir] = newNode;
+    } else {
+      const position = this.levelorderTraversal(targetNode); //returns the position of the targetNode
+      position[dir] = newNode;
+    }
+  };
+
+  /*performs level order traversal. If given any node as an argument then return that node's position else
     updates the sequence of the visited nodes*/
-    this.levelorderTraversal = function (searchNode = -1) {
-      const queue = [];
-      let currentNode = this.root;
-      queue.push(currentNode);
-      if (this.visited.length > 0) this.visited = [];
-      while (queue.length > 0) {
+  this.levelorderTraversal = function (searchNode = -1) {
+    const queue = [];
+    let currentNode = this.root;
+    queue.push(currentNode);
+    if (this.visited.length > 0) this.visited = [];
+    while (queue.length > 0) {
+      currentNode = queue.shift();
+      if (currentNode !== null) {
+        if (searchNode === -1) {
+          this.visited.push(currentNode.value);
+        }
+        if (currentNode.value === searchNode && searchNode !== -1) {
+          return currentNode;
+        }
+        if (currentNode.left !== null) {
+          queue.push(currentNode.left);
+        }
+        if (currentNode.right !== null) {
+          queue.push(currentNode.right);
+        }
+      }
+    }
+  };
+
+  this.zigzagTraversal = function () {
+    const queue = [];
+    let currentNode = this.root;
+    queue.push(currentNode);
+    let flag = 0;
+    if (this.visited.length > 0) this.visited = [];
+    while (queue.length > 0) {
+      let l = queue.length;
+      const ds = [];
+      for (let i = 0; i < l; i++) {
         currentNode = queue.shift();
         if (currentNode !== null) {
-          if (searchNode === -1) {
-            this.visited.push(currentNode.value);
-          }
-          if (currentNode.value === searchNode && searchNode !== -1) {
-            return currentNode;
-          }
+          ds.push(currentNode.value);
+
           if (currentNode.left !== null) {
             queue.push(currentNode.left);
           }
@@ -54,139 +78,114 @@ function Node(val) {
           }
         }
       }
-    };
-  
-    this.zigzagTraversal = function () {
-      const queue = [];
-      let currentNode = this.root;
-      queue.push(currentNode);
-      let flag = 0;
-      if (this.visited.length > 0) this.visited = [];
-      while (queue.length > 0) {
-        let l = queue.length;
-        const ds = [];
-        for (let i = 0; i < l; i++) {
-          currentNode = queue.shift();
-          if (currentNode !== null) {
-            ds.push(currentNode.value);
-  
-            if (currentNode.left !== null) {
-              queue.push(currentNode.left);
-            }
-            if (currentNode.right !== null) {
-              queue.push(currentNode.right);
-            }
-          }
+      if (flag == 0) {
+        for (let i = 0; i < ds.length; i++) {
+          const element = ds[i];
+          this.visited.push(element);
         }
-        if (flag == 0) {
-          for (let i = 0; i < ds.length; i++) {
-            const element = ds[i];
-            this.visited.push(element);
-          }
-  
-          flag = 1;
-        } else {
-          ds.reverse();
-          for (let i = 0; i < ds.length; i++) {
-            const element = ds[i];
-            this.visited.push(element);
-          }
-  
-          flag = 0;
+
+        flag = 1;
+      } else {
+        ds.reverse();
+        for (let i = 0; i < ds.length; i++) {
+          const element = ds[i];
+          this.visited.push(element);
         }
+
+        flag = 0;
       }
-      console.log(this.visited);
-    };
-  
-    /*Recursively performs inorder traversal and updates the sequence of visited node*/
-    this.inorderTraversal = function (node = this.root) {
-      if (node === this.root && this.visited.length > 0) this.visited = [];
-      if (node !== null) {
-        this.inorderTraversal(node.left);
-        this.visited.push(node.value);
-        this.inorderTraversal(node.right);
+    }
+    console.log(this.visited);
+  };
+
+  /*Recursively performs inorder traversal and updates the sequence of visited node*/
+  this.inorderTraversal = function (node = this.root) {
+    if (node === this.root && this.visited.length > 0) this.visited = [];
+    if (node !== null) {
+      this.inorderTraversal(node.left);
+      this.visited.push(node.value);
+      this.inorderTraversal(node.right);
+    }
+  };
+
+  /*Recursively performs preorder traversal and updates the sequence of visited node*/
+  this.preorderTraversal = function (node = this.root) {
+    if (node === this.root && this.visited.length > 0) this.visited = [];
+    if (node !== null) {
+      this.visited.push(node.value);
+      this.preorderTraversal(node.left);
+      this.preorderTraversal(node.right);
+    }
+  };
+
+  /*Recursively performs postorder traversal and updates the sequence of visited node*/
+  this.postorderTraversal = function (node = this.root) {
+    if (node === this.root && this.visited.length > 0) this.visited = [];
+    if (node !== null) {
+      this.postorderTraversal(node.left);
+      this.postorderTraversal(node.right);
+      this.visited.push(node.value);
+    }
+  };
+  // Boundary Traversal-----------------------------------
+  this.isLeaf = function (tmp = this.root) {
+    return tmp.left === null && tmp.right === null;
+  };
+  this.leftBoundary = function () {
+    let tmp = this.root.left;
+    while (tmp) {
+      if (!this.isLeaf(tmp)) {
+        this.visited.push(tmp.value);
       }
-    };
-  
-    /*Recursively performs preorder traversal and updates the sequence of visited node*/
-    this.preorderTraversal = function (node = this.root) {
-      if (node === this.root && this.visited.length > 0) this.visited = [];
-      if (node !== null) {
-        this.visited.push(node.value);
-        this.preorderTraversal(node.left);
-        this.preorderTraversal(node.right);
+      if (tmp.left) {
+        tmp = tmp.left;
+      } else {
+        tmp = tmp.right;
       }
-    };
-  
-    /*Recursively performs postorder traversal and updates the sequence of visited node*/
-    this.postorderTraversal = function (node = this.root) {
-      if (node === this.root && this.visited.length > 0) this.visited = [];
-      if (node !== null) {
-        this.postorderTraversal(node.left);
-        this.postorderTraversal(node.right);
-        this.visited.push(node.value);
+    }
+  };
+  this.rightBoundary = function () {
+    let tmp = this.root.right;
+    const ds = [];
+    while (tmp) {
+      if (!this.isLeaf(tmp)) {
+        ds.push(tmp.value);
       }
-    };
-    // Boundary Traversal-----------------------------------
-    this.isLeaf = function (tmp=this.root) {
-      return tmp.left === null && tmp.right === null;
-    };
-    this.leftBoundary = function () {
-      let tmp = this.root.left;
-      while (tmp) {
-        if (!this.isLeaf(tmp)) {
-          this.visited.push(tmp.value);
-        }
-        if (tmp.left) {
-          tmp = tmp.left;
-        } else {
-          tmp = tmp.right;
-        }
+      if (tmp.right) {
+        tmp = tmp.right;
+      } else {
+        tmp = tmp.left;
       }
-    };
-    this.rightBoundary = function () {
-      let tmp = this.root.right;
-      const ds = [];
-      while (tmp) {
-        if (!this.isLeaf(tmp)) {
-          ds.push(tmp.value);
-        }
-        if (tmp.right) {
-          tmp = tmp.right;
-        } else {
-          tmp = tmp.left;
-        }
-      }
-      ds.reverse();
-      for (let i = 0; i < ds.length; i++) {
-        const element = ds[i];
-        this.visited.push(element);
-      }
-    };
-    this.addLeaves = function (node=this.root) {
-      if (this.isLeaf(node)){
-        this.visited.push(node.value);
-      }
-      if (node.left) {
-        this.addLeaves(node.left);
-      }
-      if (node.right) {
-        this.addLeaves(node.right);
-      }
-    };
-  
-    this.boundaryTraversal = function () {
-      this.visited=[];
-      this.visited.push(this.root.value);
-      this.leftBoundary();
-      this.addLeaves();
-      this.rightBoundary();
-    };
-    this.getTraversalOrder = function () {
-      console.log(`${this.visited}`);
-      return this.visited;
-    };
-  }
-  
-  export default Tree;
-  
+    }
+    ds.reverse();
+    for (let i = 0; i < ds.length; i++) {
+      const element = ds[i];
+      this.visited.push(element);
+    }
+  };
+  this.addLeaves = function (node = this.root) {
+    if (this.isLeaf(node)) {
+      this.visited.push(node.value);
+    }
+    if (node.left) {
+      this.addLeaves(node.left);
+    }
+    if (node.right) {
+      this.addLeaves(node.right);
+    }
+  };
+
+  this.boundaryTraversal = function () {
+    this.visited = [];
+    this.visited.push(this.root.value);
+    this.leftBoundary();
+    this.addLeaves();
+    this.rightBoundary();
+  };
+  this.getTraversalOrder = function () {
+    console.log(`${this.visited}`);
+    return this.visited;
+  };
+}
+
+export default Tree;
--- src/pages/Graph/Algo/dfs.jsx
+++ src/pages/Graph/Algo/dfs.jsx
@@ -4,6 +4,7 @@ import ReactFlow, {
   Background,
   applyNodeChanges,
   applyEdgeChanges,
+  BackgroundVariant,
 } from "reactflow";
 import "reactflow/dist/style.css";
 import TransitionedList from "../../../components/TransitionList";
@@ -20,16 +21,67 @@ class Dfs extends Component {
           data: { label: "1" },
           position: { x: 0, y: 0 },
           type: "input",
-          
+          style: {
+            height: 30,
+            width: 20,
+          },
+        },
+        {
+          id: "6",
+          data: { label: "6" },
+          position: { x: 0, y: 300 },
+
+          style: {
+            height: 30,
+            width: 20,
+          },
         },
         {
           id: "2",
           data: { label: "2" },
+          position: { x: -100, y: 100 },
+          style: {
+            height: 30,
+            width: 20,
+          },
+        },
+        {
+          id: "3",
+          data: { label: "3" },
           position: { x: 100, y: 100 },
+          style: {
+            height: 30,
+            width: 20,
+          },
+        },
+        {
+          id: "5",
+          data: { label: "5" },
+          position: { x: 100, y: 200 },
+          style: {
+            height: 30,
+            width: 20,
+          },
+        },
+        {
+          id: "4",
+          data: { label: "4" },
+          position: { x: -100, y: 200 },
+          style: {
+            height: 30,
+            width: 20,
+          },
         },
       ],
       edges: [
-        { id: "1-2", source: "1", target: "2", label: "to the", type: "step" },
+        { id: "1-2", source: "1", target: "2", style: { stroke: "blue" } },
+        { id: "1-3", source: "1", target: "3", style: { stroke: "blue" } },
+        { id: "2-4", source: "2", target: "4", style: { stroke: "blue" } },
+        { id: "4-6", source: "4", target: "6", style: { stroke: "blue" } },
+        { id: "3-5", source: "3", target: "5", style: { stroke: "blue" } },
+        { id: "5-6", source: "5", target: "6", style: { stroke: "blue" } },
+        { id: "2-3", source: "2", target: "3", style: { stroke: "blue" } },
+        { id: "4-5", source: "4", target: "5", style: { stroke: "blue" } },
       ],
     };
   }
@@ -39,7 +91,7 @@ class Dfs extends Component {
       index < this.state.traversalOrder.length
     ) {
       console.log("displaylist");
-      let tempList = [...this.state.list, this.state.traversalOrder[index]];
+      let tempList = [...this.state.list, this.state.traversalOrder[index][0]];
       this.setState({ list: tempList }, () => {
         setTimeout(() => {
           this.displayList(index + 1);
@@ -52,45 +104,67 @@ class Dfs extends Component {
   animateGraph = (value) => {
     console.log(value);
     let graphData = [...this.state.nodes];
-    console.log("graphdata before animation");
-    console.log(graphData);
+
+    let edgeData = [...this.state.edges];
+
+    const updatedEdgeData = edgeData.map((edge, index) => {
+      console.log(index);
+      const ind=index.toString();
+      if (ind===value[1]) {
+        console.log("im in if block");
+        return {
+          ...edge,
+          style: { ...edge.style, stroke: "green" },
+        };
+      }
+      return edge;
+    });
 
     const updatedGraphData = graphData.map((node) => {
-      if (node.id === value) {
+      if (node.id === value[0]) {
         // Assuming `style` is an object inside the node
-        return { ...node, style: { background: "green",color:"blue" } };
+        return {
+          ...node,
+          style: { ...node.style, background: "green", color: "blue" },
+        };
       }
       return node;
     });
 
     console.log(updatedGraphData);
-    this.setState({ nodes: updatedGraphData }, () => {
+    this.setState({ nodes: updatedGraphData, edges: updatedEdgeData }, () => {
       console.log("State updated:", this.state.nodes);
     });
   };
 
   handleClick = () => {
-    const traversalOrder = ["1", "2"];
-    console.log(traversalOrder);
+    const traversalOrder = [["1",'0'],["2","6"],["3","4"], ["5","5"],["6","3"],["4","-1"]];
     this.setState({ traversalOrder }, () => {
       this.displayList(0);
-      console.log(this.state.traversalOrder[0]);
       this.animateGraph(this.state.traversalOrder[0]);
     });
   };
 
   render() {
     return (
-      <div style={{ height: 500, width: 800 }} className="flex flex-row">
-        <ReactFlow nodes={this.state.nodes} edges={this.state.edges} fitView>
-          <Background />
-          <Controls />
-        </ReactFlow>
-        <button onClick={this.handleClick}>show</button>
-        <div className="transition-list">
-          <TransitionedList list={this.state.list} />
+      <>
+        <div className="flex flex-row">
+          <div style={{ height: 500, width: 600 }} className="flex flex-row">
+            <ReactFlow
+              nodes={this.state.nodes}
+              edges={this.state.edges}
+              fitView
+            >
+              <Background variant={BackgroundVariant.Dots} />
+              <Controls />
+            </ReactFlow>
+          </div>
+          <button onClick={this.handleClick}>show</button>
+          <div className="transition-list">
+            <TransitionedList list={this.state.list} />
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 }
